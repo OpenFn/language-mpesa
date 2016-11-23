cfa33b26bc3fb9122bf6b852c8f97e99b4e7bb94
Carl Eiserman <carleiserman@gmail.com>
2016-11-23 14:50:28 +0200

    Hotfix - Removed callback from postData

--- src/Adaptor.js
+++ src/Adaptor.js
@@ -129,7 +129,6 @@ export function postData(params) {
       })
     }).then((data) => {
       const nextState = { ...state, response: { body: data } };
-      if (callback) return callback(nextState);
       return nextState;
     })
 
