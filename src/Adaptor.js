21275ba3283c87b789832f801beb433d20d99674
taylordowns2000 <downs.taylor@gmail.com>
2017-01-26 16:39:34 -0700

    fail when code != 200,201,202

--- src/Adaptor.js
+++ src/Adaptor.js
@@ -175,6 +175,12 @@ export function postData(params) {
 
   return state => {
 
+    function assembleError({ response, error }) {
+      if (response && ([200,201,202].indexOf(response.statusCode) > -1)) return false;
+      if (error) return error;
+      return new Error(`Server responded with ${response.statusCode}`)
+    }
+
     const { url, body, headers } = expandReferences(params)(state);
 
     return new Promise((resolve, reject) => {
@@ -185,8 +191,10 @@ export function postData(params) {
         json: body,
         headers
       }, function(error, response, body){
+        error = assembleError({error, response})
         if(error) {
           reject(error);
+          console.log(response);
         } else {
           console.log("Printing response...\n");
           console.log(JSON.stringify(response, null, 4) + "\n");
