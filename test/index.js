ab34d2356b0387f4aa88e0c80874dcb847af343b
Taylor Downs <downs.taylor@gmail.com>
2019-08-01 18:55:55 +0100

    write authentication tests

--- test/index.js
+++ test/index.js
@@ -6,9 +6,37 @@ import ClientFixtures, { fixtures } from './ClientFixtures';
 import Adaptor from '../src';
 const { execute, event, dataElement, get } = Adaptor;
 
+const testState = {
+  configuration: {
+    hostUrl: 'https://sandbox.safaricom.co.ke',
+    consumerKey: 'shhh',
+    consumerSecret: 'doubleShh',
+    shortCode: '601402',
+    username: 'testapi402',
+    password: 'Safaricom111!',
+    listenerUrl: 'https://www.openfn.org/inbox/abc123xyz',
+  },
+  data: {},
+};
+
 describe('execute', () => {
-  it('executes each operation in sequence', done => {
-    let state = {};
+  before(() => {
+    nock(testState.configuration.hostUrl, {
+      headers: {
+        host: 'sandbox.safaricom.co.ke',
+        authorization: 'Basic c2hoaDpkb3VibGVTaGg=',
+      },
+    })
+      .persist()
+      .get('/oauth/v1/generate?grant_type=client_credentials')
+      .reply(200, {
+        access_token: 'fakeToken',
+        expires_in: '3599',
+      });
+  });
+
+  it('authenticates and executes each operation in sequence', done => {
+    let state = testState;
     let operations = [
       state => {
         return { counter: 1 };
@@ -30,47 +58,28 @@ describe('execute', () => {
   });
 
   it('assigns references, data to the initialState', () => {
-    let state = {};
+    let state = testState;
 
     let finalState = execute()(state);
 
     execute()(state).then(finalState => {
       expect(finalState).to.eql({
         references: [],
-        data: null,
+        data: {},
+        configuration: {
+          hostUrl: 'https://sandbox.safaricom.co.ke',
+          consumerKey: 'shhh',
+          consumerSecret: 'doubleShh',
+          shortCode: '601402',
+          username: 'testapi402',
+          password: 'Safaricom111!',
+          listenerUrl: 'https://www.openfn.org/inbox/abc123xyz',
+          oAuth: {
+            access_token: 'fakeToken',
+            expires_in: '3599',
+          },
+        },
       });
     });
   });
 });
-
-// describe('registerListener', () => {
-//   before(() => {
-//     nock('https://play.http.org')
-//       .get('/demo/api/events')
-//       .reply(200, { foo: 'bar' });
-//   });
-
-//   it('calls the callback', () => {
-//     let state = {
-//       configuration: {
-//         username: 'hello',
-//         password: 'there',
-//         baseUrl: 'https://play.http.org/demo',
-//       },
-//     };
-
-//     return execute(
-//       registerListener('api/events', {
-//         callback: (response, state) => {
-//           return { ...state, references: [response] };
-//         },
-//         username: null,
-//       })
-//     )(state).then(state => {
-//       let responseBody = state.references[0].response.body;
-
-//       // Check that the eventData made it's way to the request as a string.
-//       expect(responseBody).to.eql({ foo: 'bar' });
-//     });
-//   });
-// });
